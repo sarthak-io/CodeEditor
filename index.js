6b11210029caf6fea3d156186a4ba14ea78bbdfb
sarthak-io <sarthak25ic049@satiengg.in>
2023-11-04 00:16:35 +0530

    10.0

--- index.js
+++ index.js
@@ -2,39 +2,28 @@ const express = require("express");
 const app = express();
 const bodyP = require("body-parser");
 const compiler = require("compilex");
-const cors = require("cors"); // Import the cors middleware
-const options = { stats: true }
+const cors = require("cors");
+
+const options = { stats: true };
 compiler.init(options);
 
 const allowCors = (fn) => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // You can specify the allowed origin instead of '*'
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-  
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-  
-    return await fn(req, res);
-  };
-  
-  const handler = (req, res) => {
-    const d = new Date();
-    res.end(d.toString());
-  };
-  
- module.exports = allowCors(handler);
-  
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Specify your allowed origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
-compiler.init(options)
-app.use(bodyP.json())
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  return await fn(req, res);
+};
+
+app.use(allowCors); // Use the allowCors middleware
+
+app.use(bodyP.json());
 app.post("/compile", function (req, res) {
     var code = req.body.code
     var input = req.body.input
