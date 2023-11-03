f1f86c29f28bff09a0a60e9d3c6af419161cf245
sarthak-io <sarthak25ic049@satiengg.in>
2023-11-03 23:02:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import  express from "express";
+const app = express();
+const port = 9000;
+app.use("/",(req,res)=>{
+ res.json({message:"hello from sarthak expres"});
+});
+app.listen(9000,()=>{
+    console.log("starting")
+})
