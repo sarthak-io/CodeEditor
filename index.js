d4187985627e0aba4200a4f139c8cc495b9ea42d
sarthak-io <sarthak25ic049@satiengg.in>
2023-11-03 23:23:30 +0530

    updated2.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import  express from "express";
 const app = express();
 const port = 9000;
 app.use("/",(req,res)=>{
- res.json({message:"hello from sarthak expres"});
+ res.json({message:"hello from sarthak express"});
 });
 app.listen(9000,()=>{
     console.log("starting")
