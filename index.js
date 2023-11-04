59ea8a89247afce54bf9151868b1da30079df31e
sarthak-io <sarthak25ic049@satiengg.in>
2023-11-04 11:40:24 +0530

    12.0

--- index.js
+++ index.js
@@ -3,23 +3,16 @@ const app = express();
 const bodyP = require("body-parser");
 const compiler = require("compilex");
 const cors = require("cors");
-
 const options = { stats: true };
 compiler.init(options);
 app.get("/", function (req, res) {
     compiler.flush(function () {
         console.log("deleted")
     })
-       // Manually specify the relative path to your 'index.html' file
-       const indexPath = 'index.html';
-
-       res.sendFile(indexPath, { root: __dirname });
+      const indexPath = 'index.html';
+      res.sendFile(indexPath, { root: __dirname });
     });
-   
-
-
-
-app.use(bodyP.json());
+   app.use(bodyP.json());
 app.post("/compile", function (req, res) {
     var code = req.body.code
     var input = req.body.input
@@ -79,31 +72,44 @@ app.post("/compile", function (req, res) {
         else if (lang == "Python") {
             if (!input) {
                 var envData = { OS: "windows" };
-                compiler.compilePython(envData, code, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
-            }
-            else {
+                try {
+                    compiler.compilePython(envData, code, function (data) {
+                        console.log(data.output);
+                        if (data.output) {
+                            res.send(data);
+                        } else {
+                            res.send({ output: "error404" });
+                        }
+                    });
+                } catch (error) {
+                    // Log the error when Python compilation fails
+                    console.error("Python Compilation Error:", error);
+                    res.status(500).send({ error: "An error occurred during Python compilation." });
+                }
+            } else {
                 var envData = { OS: "windows" };
-                compiler.compilePythonWithInput(envData, code, input, function (data) {
-                    if (data.output) {
-                        res.send(data);
-                    }
-                    else {
-                        res.send({ output: "error" })
-                    }
-                });
+                try {
+                    compiler.compilePythonWithInput(envData, code, input, function (data) {
+                        console.log(data.output);
+                        if (data.output) {
+                            res.send(data);
+                        } else {
+                            res.send({ output: "error404" });
+                        }
+                    });
+                } catch (error) {
+                    // Log the error when Python compilation with input fails
+                    console.error("Python Compilation with Input Error:", error);
+                    res.status(500).send({ error: "An error occurred during Python compilation with input." });
+                }
             }
         }
+        
     }
-    catch (e) {
-        console.log("error")
+    catch (error) {
+    console.error("Compilation Error:", error);
+    // Optionally, you can send an error response to the client
+    res.status(500).send({ error: "An error occurred during compilation." });
     }
 })
-
 app.listen(8000)
